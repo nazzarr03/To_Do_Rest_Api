3c748ae073e1677b3d1d715cfcfa6f062240fa30
nazzarr03 <nazararik585@gmail.com>
2024-05-14 19:56:04 +0300

    Added DeleteToDoList function.

--- repository/toDoListRepository.go
+++ repository/toDoListRepository.go
@@ -22,9 +22,9 @@ func (toDoListArray *ToDoListArray) CreateToDoList(userID uint, toDoList models.
 	toDoListArray.mutex.Lock()
 	defer toDoListArray.mutex.Unlock()
 
-	UserArray := UserArray{}
+	userArray := UserArray{}
 
-	for _, user := range UserArray.users {
+	for _, user := range userArray.users {
 		if user.UserID == userID {
 			toDoList.UserID = userID
 		} else {
@@ -39,4 +39,32 @@ func (toDoListArray *ToDoListArray) CreateToDoList(userID uint, toDoList models.
 	toDoListArray.toDoLists = append(toDoListArray.toDoLists, toDoList)
 
 	return toDoList, nil
+}
+
+func (toDoListArray *ToDoListArray) DeleteToDoList(listID, userID uint) error {
+	toDoListArray.mutex.Lock()
+	defer toDoListArray.mutex.Unlock()
+
+	userArray := UserArray{}
+
+	for i, toDoList := range toDoListArray.toDoLists {
+		for _, user := range userArray.users {
+			if toDoList.ListID == listID {
+				if user.UserID == userID {
+					if user.UserType == "admin" || user.UserID == toDoList.UserID {
+						toDoListArray.toDoLists[i].DeletedAt = time.Now()
+						return nil
+					} else {
+						return errors.New("you are not authorized to delete this to-do list")
+					}
+				} else {
+					return errors.New("user not found")
+				}
+			} else {
+				return errors.New("to-do list not found")
+			}
+		}
+	}
+
+	return nil
 }
