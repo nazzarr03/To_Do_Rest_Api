650a3cc2f32886f41d0a5e99c56a72f976ec5bb7
nazzarr03 <nazararik585@gmail.com>
2024-05-14 19:43:01 +0300

    Added CreateToDoList function.

--- repository/toDoListRepository.go
+++ repository/toDoListRepository.go
@@ -1,7 +1,9 @@
 package repository
 
 import (
+	"errors"
 	"sync"
+	"time"
 
 	"github.com/nazzarr03/To-Do-Rest-Api/models"
 )
@@ -14,4 +16,27 @@ type ToDoListArray struct {
 type ToDoListRepository interface {
 	CreateToDoList(userID uint, toDoList models.ToDoList) (models.ToDoList, error)
 	DeleteToDoList(listID, userID uint) error
+}
+
+func (toDoListArray *ToDoListArray) CreateToDoList(userID uint, toDoList models.ToDoList) (models.ToDoList, error) {
+	toDoListArray.mutex.Lock()
+	defer toDoListArray.mutex.Unlock()
+
+	UserArray := UserArray{}
+
+	for _, user := range UserArray.users {
+		if user.UserID == userID {
+			toDoList.UserID = userID
+		} else {
+			return models.ToDoList{}, errors.New("user not found")
+		}
+	}
+
+	toDoList.ListID = uint(toDoListArray.toDoLists[len(toDoListArray.toDoLists)-1].ListID) + 1
+	toDoList.CompletionPercent = 0
+	toDoList.CreatedAt = time.Now()
+
+	toDoListArray.toDoLists = append(toDoListArray.toDoLists, toDoList)
+
+	return toDoList, nil
 }
