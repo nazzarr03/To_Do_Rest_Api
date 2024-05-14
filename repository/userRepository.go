687785c39bf039de18f403d26599cdb9f27b04fa
nazzarr03 <nazararik585@gmail.com>
2024-05-14 19:12:42 +0300

    Added Login function.

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/nazzarr03/To-Do-Rest-Api/models"
+import (
+	"errors"
+
+	"github.com/nazzarr03/To-Do-Rest-Api/models"
+	"github.com/nazzarr03/To-Do-Rest-Api/utils"
+)
 
 type UserArray struct {
 	users []models.User
@@ -20,4 +25,19 @@ func AddUser() {
 		Password: "password2",
 		UserType: "admin",
 	})
+}
+
+func (userArray *UserArray) Login(user models.User) (string, error) {
+	for _, u := range userArray.users {
+		if u.Username == user.Username && u.Password == user.Password {
+			token, err := utils.GenerateToken(u.UserID)
+			if err != nil {
+				return "", err
+			}
+
+			return token, nil
+		}
+	}
+
+	return "", errors.New("invalid username or password")
 }
