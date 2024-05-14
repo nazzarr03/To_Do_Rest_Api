ed4c118605f0c8434bb91aa109c4fbc9557b8c27
nazzarr03 <nazararik585@gmail.com>
2024-05-14 19:04:17 +0300

    Added GenerateToken and ValidateToken functions in the jwt file.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nazzarr03/To-Do-Rest-Api
 
 go 1.21.9
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible
---
+++ utils/jwt.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var jwtSecret = []byte("SecretKey")
+
+type JWTClaims struct {
+	UserId uint `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(userID uint) (string, error) {
+	claims := &JWTClaims{
+		UserId: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * 24).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateToken(token string) (*JWTClaims, error) {
+	jwtToken, err := jwt.ParseWithClaims(
+		token,
+		&JWTClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := jwtToken.Claims.(*JWTClaims)
+	if !ok {
+		return nil, errors.New("invalid JWT claims")
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		return nil, errors.New("token is expired")
+	}
+
+	return claims, nil
+}
